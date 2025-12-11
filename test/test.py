8e339773fae0ed85507bedf431e832d48019e41d
juemengxin <601224464@qq.com>
2025-12-11 15:49:19 +0800

    test

--- test/test.py
+++ test/test.py
@@ -4,19 +4,12 @@ while i <= 5:
 # 缺少i += 1，i永远等于1，循环无法终止
 
 
-a = 10
-b = "20"
-print(a + int(b))  # 转换为相同类型（数字）
-# 或
-print(str(a) + b)  # 转换为相同类型（字符串）
-
-
 # 错误代码
-print("Hello, Python')  # 单引号开头，双引号结尾
+print("Hello, Python")  # 单引号开头，双引号结尾
       
 # 错误代码：想判断年龄是否在18-60之间，结果逻辑相反
 age = 25
-if age < 18 and age >60:
+if age >= 18 and age <60:
     print("成年且未退休")
 else:
     print("未成年或已退休")
